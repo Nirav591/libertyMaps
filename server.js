3ff6289355da3db7a953bc21ff957fcb6086f186
Nirav Lathiya <83869023+Nirav591@users.noreply.github.com>
2024-07-05 11:40:19 +0530

    added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const db = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const otpRoutes = require('./routes/otpRoutes');
+const otpRoutes = require('./routes/userRoutes');
 require('dotenv').config();
 
 const app = express();
