4f5534c1f0c3d8be205107f456d6f591d23de75c
Nirav Lathiya <83869023+Nirav591@users.noreply.github.com>
2024-07-06 14:56:00 +0530

    added

--- config/db.js
+++ config/db.js
@@ -1,16 +1,31 @@
 const mysql = require('mysql2');
-require('dotenv').config();
-
-const db = mysql.createConnection({
+const config = {
     host: 'localhost',
     user: 'admin',
     password:  't7x?}>rbmCa~we+',
     database: 'libertymaps',
-});
+};
 
-db.connect((err) => {
-    if (err) throw err;
-    console.log('Connected to database');
-});
+let connection;
 
-module.exports = db;
+function handleDisconnect() {
+    connection = mysql.createConnection(config);
+
+    connection.connect(err => {
+        if (err) {
+            console.error('Error connecting to the database:', err);
+            setTimeout(handleDisconnect, 2000); // Retry connection after 2 seconds
+        }
+    });
+
+    connection.on('error', err => {
+        console.error('Database error:', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            handleDisconnect(); // Reconnect if connection is lost
+        } else {
+            throw err;
+        }
+    });
+}
+
+handleDisconnect();
