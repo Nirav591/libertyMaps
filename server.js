77eab0954f9f4288ef944e83863f973dc6e3b39b
Nirav Lathiya <83869023+Nirav591@users.noreply.github.com>
2024-07-08 14:52:09 +0530

    added

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const db = require('./config/db');
+const cors = require('cors');
+
+const app = express();
 const authRoutes = require('./routes/authRoutes');
 const otpRoutes = require('./routes/otpRoutes');
 const userRoutes = require('./routes/userRoutes');
@@ -7,16 +9,33 @@ const companyRoutes = require('./routes/companyRoutes');
 
 require('dotenv').config();
 
+// List of allowed origins
+const allowedOrigins = ['http://localhost:3000', 'https://yourproductiondomain.com'];
 
-const app = express();
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Check if the origin is in the allowedOrigins list or if there's no origin (for non-browser requests)
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // if you need to send cookies with the requests
+};
+
+// Use CORS middleware with the defined options
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 app.use('/auth', authRoutes);
 app.use('/otp', otpRoutes);
 app.use('/api', userRoutes);
-app.use('/companies' , companyRoutes)
+app.use('/companies', companyRoutes);
 
-const PORT = 6360;
+const PORT = process.env.PORT || 6360;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
